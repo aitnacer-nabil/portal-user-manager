f116f804e18c06158ded257f13648bfe2e377c87
aitnacer-nabil <palancia.gor@gmail.com>
2024-02-18 12:54:34 +0100

    JwtAccessDenieHandler

--- src/main/java/com/nabilaitnacer/portalusermanager/filter/JwtAccessDenieHandler.java
+++ src/main/java/com/nabilaitnacer/portalusermanager/filter/JwtAccessDenieHandler.java
@@ -1,4 +1,33 @@
 package com.nabilaitnacer.portalusermanager.filter;
 
-public class JwtAccessDenieHandler {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nabilaitnacer.portalusermanager.domain.HttpResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static com.nabilaitnacer.portalusermanager.constant.SecurityConstant.ACCESS_DENIED_MESSAGE;
+
+public class JwtAccessDenieHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        HttpResponse httpResponse = HttpResponse.builder()
+                .httpStatus(HttpStatus.UNAUTHORIZED.toString())
+                .httpStatusCode(HttpStatus.UNAUTHORIZED.value())
+                .reason(HttpStatus.UNAUTHORIZED.getReasonPhrase().toUpperCase())
+                .message(ACCESS_DENIED_MESSAGE)
+                .build();
+        response.setContentType("application/json");
+        response.setStatus(httpResponse.getHttpStatusCode());
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(outputStream, httpResponse);
+        outputStream.flush();
+    }
 }
--- src/main/java/com/nabilaitnacer/portalusermanager/filter/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/nabilaitnacer/portalusermanager/filter/JwtAuthenticationEntryPoint.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import static com.nabilaitnacer.portalusermanager.constant.SecurityConstant.FORBIDEN_MESSAGE;
+import static com.nabilaitnacer.portalusermanager.constant.SecurityConstant.FORBIDDEN_MESSAGE;
 
 public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint{
     @Override
@@ -20,7 +20,7 @@ public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint{
                         .httpStatus(HttpStatus.FORBIDDEN.toString())
                         .httpStatusCode(HttpStatus.FORBIDDEN.value())
                         .reason(HttpStatus.FORBIDDEN.getReasonPhrase().toUpperCase())
-                        .message(FORBIDEN_MESSAGE)
+                        .message(FORBIDDEN_MESSAGE)
                         .build();
         response.setContentType("application/json");
         response.setStatus(httpResponse.getHttpStatusCode());
